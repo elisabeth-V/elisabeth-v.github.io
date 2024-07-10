c2191597ce252685937d1ef4ab7fa6618f962b6c
elisabeth-V <139359279+elisabeth-V@users.noreply.github.com>
2024-07-10 14:00:08 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -9,10 +9,9 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
-  <App />
+  <router>
+      <App />
+  </router>
 );
 
 // If you want to start measuring performance in your app, pass a function
