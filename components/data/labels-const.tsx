298ae2414522a8f2af479dc97660b1a86b2b0ba7
elisabeth-V <139359279+elisabeth-V@users.noreply.github.com>
2024-07-15 10:13:35 +0100

    Delete components directory

--- components/data/labels-const.tsx
+++
@@ -1,7 +0,0 @@
-export const MyLabels = {
-    subTitle :'SPA using Typescript',
-    overallMessage : 'Welcome to my website',
-    loadingMessage:"Loading !!!",
-    errorMessage: "Oops something went wrong !"
-}
-
